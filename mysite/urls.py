38b5ccd9cd33e107257ac92efdc5ea83900baa3d
kisilevandrey <kisilevandrey@gmail.com>
2016-04-01 22:31:37 +0300

    Changed the html files

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 from mysite.views import hello
 from mysite.views import current_datetime
@@ -24,11 +24,11 @@ def hey(request): # счетчик количества запросов
     global a
     a = a+1
     return HttpResponse(a)
-
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
+    url(r'^admin/', include(admin.site.urls),
     url('^hello/$', hello),
     url('^time/$', current_datetime),
-    url(r'^counter/$', hey)
+    url(r'^counter/$', hey),
 ]
 
